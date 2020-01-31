c8b6f0a4bf3e1e500a6835e5fb477398d8c2c9cf
Anthony <ach0074@gmail.com>
2020-01-31 16:49:05 -0500

    Added comment functionality

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@ let commentCount = 1;
 const commentCounter = document.querySelector('.comment-counter');
 const btn = document.getElementById('btn');
 const modal = document.getElementById('text-modal');
+const userName = document.getElementById('userName');
+const msg = document.getElementById('msg');
 const submit = document.getElementById('submit');
 const cancel = document.getElementById('cancel');
 
@@ -11,12 +13,34 @@ function openModal() {
 
 function closeModal() {
   modal.style.display = "none";
+  userName.value = '';
+  msg.value = '';
+  userName.placeholder = '';
+  msg.placeholder = '';
+}
+
+function createElement() {
+  let newElement = document.createElement('div');
+    newElement.className = 'comment';
+    newElement.innerHTML = `
+    <span class="user"><i class="material-icons">person</i>${userName.value}</span>
+    <p>${msg.value}</p>
+    `
+    document.getElementById('bottom-container').insertAdjacentElement('beforeend', newElement);
 }
 
 function addComment() {
-  commentCount++;
-  commentCounter.innerHTML = '('+ commentCount + ')'
-  closeModal();
+  if (userName.value != '' && msg.value != '') {
+    console.log("The user is " + userName.value);
+    createElement();
+    closeModal();
+    commentCount++;
+    commentCounter.innerHTML = '('+ commentCount + ')';    
+  } else {
+    userName.placeholder = "This field is required before submitting";
+    msg.placeholder = "This field is required before submitting";
+    console.log("need fields filled");
+  }
 }
 
 btn.addEventListener('click', openModal);
