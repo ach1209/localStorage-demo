8d896bb9ad01ba5e365592b0cd948e166f4681ef
Anthony <ach0074@gmail.com>
2020-02-03 18:34:16 -0500

    Code clean up

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-let commentCount = 1;
 const commentCounter = document.querySelector('.comment-counter');
 const btn = document.getElementById('btn');
 const modal = document.getElementById('text-modal');
@@ -6,19 +5,13 @@ const userName = document.getElementById('userName');
 const msg = document.getElementById('msg');
 const submit = document.getElementById('submit');
 const cancel = document.getElementById('cancel');
+let commentCount = 1;
 let getAllComments = JSON.parse(localStorage.getItem('allComments'));
 
 window.onload = () => {
   if (getAllComments) {
-    console.log(getAllComments);
     getAllComments.forEach(comment => {
-      let newElement = document.createElement('div');
-      newElement.className = 'comment';
-      newElement.innerHTML = `
-      <span class="user"><i class="material-icons">person</i>${comment.user}</span>
-      <p>${comment.msg}</p>
-      `
-    document.getElementById('bottom-container').insertAdjacentElement('beforeend', newElement);
+      createElement(comment.user, comment.msg);
     });
     commentCount += getAllComments.length;
     commentCounter.innerHTML = '('+ commentCount + ')';    
@@ -42,34 +35,32 @@ function AddedComment(user, msg) {
   this.msg = msg
 }
 
-function createElement() {
-  let newComment = new AddedComment(userName.value, msg.value);
-  if (getAllComments == null) {
-    getAllComments = [];
-  }
-  getAllComments.push(newComment);
-  localStorage.setItem('allComments', JSON.stringify(getAllComments));
-
+function createElement(name, message) {
   let newElement = document.createElement('div');
       newElement.className = 'comment';
       newElement.innerHTML = `
-      <span class="user"><i class="material-icons">person</i>${newComment.user}</span>
-      <p>${newComment.msg}</p>
+      <span class="user"><i class="material-icons">person</i>${name}</span>
+      <p>${message}</p>
       `
   document.getElementById('bottom-container').insertAdjacentElement('beforeend', newElement);
 }
 
 function addComment() {
   if (userName.value != '' && msg.value != '') {
-    console.log("The user is " + userName.value);
-    createElement();
-    closeModal();
+    let newComment = new AddedComment(userName.value, msg.value);
+    if (getAllComments == null) {
+      getAllComments = [];
+    }
+    getAllComments.push(newComment);
+    localStorage.setItem('allComments', JSON.stringify(getAllComments));
+  
+    createElement(newComment.user, newComment.msg);
+    toggleModal();
     commentCount++;
     commentCounter.innerHTML = '('+ commentCount + ')';    
   } else {
     userName.placeholder = "This field is required before submitting";
     msg.placeholder = "This field is required before submitting";
-    console.log("need fields filled");
   }
 }
 
